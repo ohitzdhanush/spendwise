83fce71792df02236c4c499a964f66684d9bef66
Dhanush Kumar <mdhanush961@gmail.com>
2026-05-01 20:25:29 +0530

    add home controller

---
+++ src/main/java/com/spendwise/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.spendwise.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spendwise Backend Running 🚀";
+    }
+}
